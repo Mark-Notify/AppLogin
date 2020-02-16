cd1e5afaf863543a64f06ef54f04cb714db66bfa
Mark <atchareeyak9@gmail.com>
2020-02-16 16:07:58 +0700

    login

--- login/login.php
+++ login/login.php
@@ -2,17 +2,21 @@
 <?php 
 	include_once('conn.php');
 	
-	// รับค่า email, password
-	$email = $_POST["email"];
-	$password = $_POST["password"];
+	if (!empty($_POST["email"]) || !empty($_POST["password"])) 
+	{
+		// รับค่า email, password
+		$email = $_POST["email"];
+		$password = $_POST["password"];
 
-	// คำสั่ง sql ที่ใช้ในการ Login
-	$sql = "SELECT * FROM student WHERE  email = '$email' AND password = '$password'";
-	$result = mysqli_query($con,$sql);
-	
-	if($result->num_rows > 0){
-		echo "login successfully" ;
-	}else{
-		echo "User not found";
+		// คำสั่ง sql ที่ใช้ในการ Login
+		$sql = "SELECT * FROM student WHERE  email = '$email' AND password = '$password'";
+		$result = mysqli_query($con,$sql);
+		
+		if($result->num_rows > 0){
+			echo "login successfully" ;
+		}else{
+			echo "User not found";
+		}
 	}
+	
 ?>
